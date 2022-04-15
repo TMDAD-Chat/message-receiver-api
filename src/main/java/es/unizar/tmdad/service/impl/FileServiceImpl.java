c67b318c7c1bd83efe5a140a0c3319bfc15c23b6
FreddyMartinez <fhmartinezq@gmail.com>
2022-04-15 21:43:34 +0200

    File service created

---
+++ src/main/java/es/unizar/tmdad/service/impl/FileServiceImpl.java
@@ -0,0 +1,41 @@
+package es.unizar.tmdad.service.impl;
+
+import es.unizar.tmdad.service.FileService;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+
+@Service
+public class FileServiceImpl implements FileService {
+
+    private final WebClient webClient;
+    private final ReactiveCircuitBreaker saveFileCircuitBreaker;
+
+    public FileServiceImpl(ReactiveCircuitBreakerFactory circuitBreakerFactory) {
+        this.webClient = WebClient.builder()
+                .baseUrl("http://localhost:8080")
+                .defaultHeader(MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .build();
+        this.saveFileCircuitBreaker = circuitBreakerFactory.create("slow");
+    }
+
+    @Override
+    public Mono<String> store(MultipartFile file, String name) {
+        System.out.println(name);
+        return saveFileCircuitBreaker.run(
+                webClient.post().uri("/")
+                        .body(BodyInserters
+                                .fromMultipartData("chat", name)
+                                .with("file", file.getResource()))
+                        .retrieve()
+                        .bodyToMono(String.class),
+                throwable -> Mono.just("Default image")
+        );
+    }
+}
